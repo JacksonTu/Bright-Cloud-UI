f1ff6d426e437ccbd1f4003b76daa05ab5747abc
JacksonTu <tuminglong@126.com>
2020-10-22 16:38:56 +0800

    优化

--- src/settings.js
+++ src/settings.js
@@ -3,22 +3,22 @@ module.exports = {
   // 获取令牌时，请求头信息(Basic Base64.encode(client_id:client_secret))
   authorizationValue: 'Basic YnJpZ2h0OjEyMzQ1Ng==',
   // 社交登录后台地址
-  socialLoginUrl: 'http://localhost:8002/auth/social/login',
+  socialLoginUrl: 'http://59.110.223.220:8002/auth/social/login',
   // 请替换为您的实际地址
   pages: {
     // Bright-Admin 控制台地址
-    springBootAdminUrl: 'http://localhost:8006/login',
+    springBootAdminUrl: 'http://59.110.223.220:8006/login',
     // kibana 控制台地址
     kibanaUrl: 'http://localhost:9400',
     // nacos 控制台地址
-    nacosUrl: 'http://localhost:8848/nacos',
+    nacosUrl: 'http://106.13.130.94:8848/nacos',
     // skywalking地址
     skywalkingUrl: 'http://localhost:9080/',
     // 文档中心
-    docUrl: 'http://localhost:8002/doc.html',
+    docUrl: 'http://59.110.223.220:8002/doc.html',
     // Granfana控制台
     grafanaUrl: 'http://localhost:9070/',
     // 消息推送 websocket
-    msgWsUrl: 'http://localhost:8004/'
+    msgWsUrl: 'http://59.110.223.220:8004'
   }
 }
